b324c5e9ce4c0a094e8357a56566b3007ff97f99
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-17 21:27:25 +0100

    [set-up]: Add: Books Component

---
+++ src/Components/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from './BookList';
+import AddNewBook from './AddNewBook';
+
+const Books = () => (
+  <>
+    <BookList />
+    <AddNewBook />
+  </>
+);
+
+export default Books;
