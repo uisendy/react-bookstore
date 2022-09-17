392d5d139925b2e746cd9d2b37119801d50e73ca
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-17 21:27:14 +0100

    [set-up]: Add: BookList Component

---
+++ src/Components/BookList.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Book from './Book';
+const BookList = () => {
+  const [books, setBooks] = useState([
+    { id: uuidv4(), title: 'Mallam Idris', author: 'Kenneth Abas' },
+    {
+      id: uuidv4(),
+      title: 'Ibibio: The people and Culture',
+      author: 'Ekpo Otu',
+    },
+    { id: uuidv4(), title: 'The Cold War', author: 'Koloviski Kollon' },
+  ]);
+
+  return (
+    <ul>
+      {books.map((book) => (
+        <Book key={book.id} title={book.title} author={book.author} />
+      ))}
+    </ul>
+  );
+};
+export default BookList;
