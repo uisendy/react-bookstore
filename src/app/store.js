c3c6012b5c86ac547dacc78245c4f04ead411338
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 06:57:28 +0100

    [feature-redux]: Add: Add BooksReducer to Store

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,9 @@ import booksReducer from '../redux/books/booksSlice';
 import categoriesReducer from '../redux/categories/categoriesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: booksReducer,
+  },
 });
 
 export default store;
