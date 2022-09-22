d622ee641915f7f5620cffc6c66d78e50e6330eb
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 07:06:18 +0100

    [feature-redux]: configure dispatch payload and update state

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,18 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAllBooks } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const [books, setBooks] = useState([
-    { id: uuidv4(), title: 'Mallam Idris', author: 'Kenneth Abas' },
-    {
-      id: uuidv4(),
-      title: 'Ibibio: The people and Culture',
-      author: 'Ekpo Otu',
-    },
-    { id: uuidv4(), title: 'The Cold War', author: 'Koloviski Kollon' },
-  ]);
+  const books = useSelector(selectAllBooks);
 
   return (
     <ul>
