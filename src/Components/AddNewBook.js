ec37be66d16f0fa2769cecd3f92b022f29104193
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 07:05:23 +0100

    [feature-redux]: configure dispatch payload and update state

--- src/Components/AddNewBook.js
+++ src/Components/AddNewBook.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BOOK_ADDED } from '../redux/books/booksSlice';
 
-const AddNewBook = () => (
-  <form action="#">
-    <h3>Add new book</h3>
-    <input type="text" className="title" placeholder="book title" />
-    <input type="text" className="author" placeholder="book author" />
-    <button type="submit" className="addBtn">
-      add book
-    </button>
-  </form>
-);
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onAuthorChanged = (e) => setAuthor(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      dispatch(BOOK_ADDED(title, author));
+
+      setTitle('');
+      setAuthor('');
+    }
+  };
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <h3>Add new book</h3>
+      <input
+        type="text"
+        className="title"
+        placeholder="book title"
+        value={title.value}
+        onChange={(e) => onTitleChanged(e)}
+      />
+      <input
+        type="text"
+        className="author"
+        placeholder="book author"
+        value={author}
+        onChange={(e) => onAuthorChanged(e)}
+      />
+      <button type="submit" className="addBtn">
+        add book
+      </button>
+    </form>
+  );
+};
 
 export default AddNewBook;
