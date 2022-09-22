5815e53eb692377eef67128eb3028277d723fa9d
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 15:10:35 +0100

    [f-react-redux]: Add react-redux provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './app/store';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
