6eb003a3b40d9380c292777693baef6d78e27a6a
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 07:05:47 +0100

    [feature-redux]: update Props

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Book = ({ title, author }) => (
-  <div className="book-container">
-    <h2>{title}</h2>
-    <p>{author}</p>
-    <button type="submit">delete</button>
-  </div>
+  <>
+    <li className="book-container">
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <button type="submit">delete</button>
+    </li>
+  </>
 );
 
 export default Book;
