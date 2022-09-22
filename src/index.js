5a6a99db8e4998800197e53d8d2c79841e8331bd
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 15:11:19 +0100

    [f-react-redux]: update react-redux provider to point to redux store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './app/store';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
