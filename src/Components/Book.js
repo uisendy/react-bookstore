a1cd5a43aaf8181845007d7340763fc5941dc935
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 10:49:03 +0100

    [feature-redux]: add: handleDeleteBook action and update props with BookId

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { BOOK_DELETED } from '../redux/books/booksSlice';
 
-const Book = ({ title, author }) => (
-  <>
-    <li className="book-container">
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <button type="submit">delete</button>
-    </li>
-  </>
-);
+const Book = ({ bookId, title, author }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = () => {
+    dispatch(BOOK_DELETED({ bookId }));
+  };
+
+  return (
+    <>
+      <li className="book-container">
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <button type="button" onClick={handleDeleteBook}>
+          delete
+        </button>
+      </li>
+    </>
+  );
+};
 
 export default Book;
