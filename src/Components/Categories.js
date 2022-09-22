2f9c91e87a214835b8e31bcfb8421083de4969fe
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-22 07:34:15 +0100

    [feature-redux]: configure categoriesSlice from redux createSlice, export actions and reducers

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  CATEGORY,
+  selectAllCategories,
+} from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <div>
-    <button type="submit">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+
+  const categories = useSelector(selectAllCategories);
+
+  const handleCheckStatus = () => {
+    dispatch(
+      CATEGORY({
+        value: 'Under Construction',
+      }),
+    );
+  };
+  return (
+    <div>
+      <button type="button" onClick={handleCheckStatus}>
+        Check status
+      </button>
+      <p>{categories[0]?.value}</p>
+    </div>
+  );
+};
 
 export default Categories;
