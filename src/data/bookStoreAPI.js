a3c98b66ce9a74f44c26297280b249737a33df0f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-23 15:15:12 +0100

    [feature-API]: Add; add API management component

---
+++ src/data/bookStoreAPI.js
@@ -0,0 +1,10 @@
+const APP_ID = 'ebUlu9czqwiIYoRChEkK';
+const BASE_URL =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+const urlMAP = {
+  postURL: `${BASE_URL}${APP_ID}/books`,
+  getURL: `${BASE_URL}${APP_ID}/books`,
+};
+
+export default urlMAP;
