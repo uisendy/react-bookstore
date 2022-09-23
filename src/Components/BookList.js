88bad8ab4a5fea696f98241fde6d74e083067323
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-23 15:14:49 +0100

    [feature-API]: update: Adjust code to dispatch fetchBooks action as side effects

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,23 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectAllBooks } from '../redux/books/booksSlice';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import {
+  getAllBooks,
+  getError,
+  getStatus,
+  fetchBooks,
+} from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const books = useSelector(selectAllBooks);
+  const dispatch = useDispatch();
+  const books = useSelector(getAllBooks);
+  const status = useSelector(getStatus);
+  const error = useSelector(getError);
 
-  return (
-    <ul>
-      {books.map((book) => (
-        <Book
-          key={book.id}
-          bookId={book.id}
-          title={book.title}
-          author={book.author}
-        />
-      ))}
-    </ul>
-  );
+  useEffect(() => {
+    console.log(books);
+    console.log(status);
+    if (status === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [status, dispatch]);
+
+  let content;
+  if (status === 'Loading') {
+    console.log('loading');
+    content = <p>&quot,loading...&quot,</p>;
+  } else if (status === 'succeeded') {
+    console.log('succeeded');
+    content = (
+      <ul>
+        {books.map((book) => (
+          <Book key={nanoid()} book={book} />
+        ))}
+      </ul>
+    );
+    console.log('succeeded DOM');
+  } else if (status === 'failed') {
+    content = <p>{error}</p>;
+  }
+
+  return <div>{content}</div>;
 };
 export default BookList;
