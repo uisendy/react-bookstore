84788fae5d9ed6e86ad7b4172825d75536f7ed43
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-24 18:14:35 +0100

    [feature-style]: Add basic style to The book sections.

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../redux/books/booksSlice';
 
-const Book = ({ bookId, title, author }) => {
+const Book = ({ bookId, title, author, category }) => {
   const dispatch = useDispatch();
 
   const handleDeleteBook = () => {
@@ -11,12 +11,18 @@ const Book = ({ bookId, title, author }) => {
 
   return (
     <>
-      <li className="book-container">
-        <h2>{title}</h2>
-        <p>{author}</p>
-        <button type="button" onClick={handleDeleteBook}>
-          delete
-        </button>
+      <li className="book-container bg-white rounded border-solid border h-52 md:h-40 mx-[6.5%] md:mx-[8.5%] mb-0 mt-10 pb-4 pt-4 px-6 md:pb-6 md:pt-6 md:pr-40">
+        <div>
+          <small className=" opacity-50 font-Montserrat text-sm font-bold tracking-normal text-secondary-gray mb-10">
+            {category}
+          </small>
+          <h2 className=" font-RobotoSlab text-xl font-bold mb-0 ml-0 mt-1 not-italic tracking-tight text-secondary-gray">
+            {title}
+          </h2>
+          <p className="font-RobotoSlab font-light text-sm mx-0 mb-0 mt-1 text-secondary-blue">
+            {author}
+          </p>
+        </div>
       </li>
     </>
   );
