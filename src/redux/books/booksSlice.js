99be075c6f5711a201b1ae228c394687847ddc4d
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-24 08:48:41 +0100

    [feature-API]: Update; update initial state to Books, status and error and javascript object

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,35 +1,10 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import urlMAP from '../../data/bookStoreAPI';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: {},
   status: 'idle',
   error: null,
 };
-
-export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
-  try {
-    const response = await axios.get(urlMAP.getURL);
-    const data = Object.entries(response.data).map(([key, value]) => ({
-      id: key,
-      title: value[0].title,
-      author: value[0].author,
-    }));
-    console.log(data);
-    return data;
-  } catch (err) {
-    throw new Error(err);
-  }
-});
-
-export const postNewBook = createAsyncThunk('books/postNewBook', async () => {
-  try {
-    const response = await axios.post(urlMAP.getURL);
-  } catch (error) {
-    throw new Error(err);
-  }
-});
 
 const booksSlice = createSlice({
   name: 'books',
@@ -37,8 +12,7 @@ const booksSlice = createSlice({
   reducers: {
     BOOK_ADDED: {
       reducer(state, action) {
-        console.log(action.payload);
-        state.books.push(action.payload);
+        state.push(action.payload);
       },
       prepare(title, author) {
         return {
@@ -52,33 +26,14 @@ const booksSlice = createSlice({
     },
     BOOK_DELETED: {
       reducer(state, action) {
-        console.log(action.payload);
-        const bookId = action.payload;
-        console.log(typeof bookId);
-        return state.books.filter((book) => book.id !== bookId);
+        const { bookId } = action.payload;
+        return state.filter((book) => book.id !== bookId);
       },
     },
   },
-  extraReducers(builder) {
-    builder
-      .addCase(fetchBooks.pending, (state, action) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchBooks.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.books = state.books.concat(action.payload);
-      })
-      .addCase(fetchBooks.rejected, (state, action) => {
-        state.status = 'failed';
-
-        state.error = action.error.message;
-      });
-  },
 });
 
-export const getAllBooks = (state) => state.books.books;
-export const getStatus = (state) => state.books.status;
-export const getError = (state) => state.books.error;
+export const selectAllBooks = (state) => state.books;
 
 export const { BOOK_ADDED, BOOK_DELETED } = booksSlice.actions;
 
