414d3f8399faf6bf8a02a47de67f67879e491c1d
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-24 08:49:37 +0100

    [feature-API]: Add: URL links for Post, get and delete Axios Methods

--- src/data/bookStoreAPI.js
+++ src/data/bookStoreAPI.js
@@ -1,10 +1,9 @@
-const APP_ID = 'ebUlu9czqwiIYoRChEkK';
+const APP_ID = 'opmhk55Jw3g3wFOeOjXp';
 const BASE_URL =
   'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
 
-const urlMAP = {
-  postURL: `${BASE_URL}${APP_ID}/books`,
-  getURL: `${BASE_URL}${APP_ID}/books`,
-};
+const URL = `${BASE_URL}${APP_ID}/books`;
 
-export default urlMAP;
+export default URL;
+
+//https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/opmhk55Jw3g3wFOeOjXp/books
