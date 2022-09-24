8a828ab6efadcc594147b234025bb2b0b025afbe
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-24 22:36:26 +0100

    [feature-style]:Html scemantics code cleaning

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
-
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
